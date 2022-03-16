718a89827da7d176171108f155524b3e102a253e
Max Calzada Munoz <70862146+maxacm@users.noreply.github.com>
2022-03-16 14:58:45 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is John Doe
-# My favorite color is blue
+# My name is Max Calzada
 # This is my coding that generates Boxplots for transmission types
+
+#The data source is the mpg data table.
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
