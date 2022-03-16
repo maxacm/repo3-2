e79b15adecebcba009f74a9073789bc8d53633d2
Max Calzada Munoz <70862146+maxacm@users.noreply.github.com>
2022-03-16 15:07:41 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Max Calzada
+# My name is Max Calzada.
 # This is my coding that generates Boxplots for transmission types
 
 #The data source is the mpg data table.
